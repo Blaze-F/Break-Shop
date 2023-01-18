9d129a9dab8496d097813a66d4eabee221c991b4
JunH <101803254+Blaze-F@users.noreply.github.com>
2023-01-18 20:51:42 +0900

    [ADD] 설정값 변경

--- breakshop/src/main/java/com/project/breakshop/accounts/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.breakshop.accounts.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository {
-}
---
+++ breakshop/src/main/java/com/project/breakshop/models/User.java
@@ -0,0 +1,9 @@
+package com.project.breakshop.models;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+public class User {
+}
